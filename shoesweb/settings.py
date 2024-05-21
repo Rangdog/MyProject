15343d4fa1fc6441775f27ff8a8164dc6fdf06c6
Rangdog <92283489+Rangdog@users.noreply.github.com>
2024-05-21 20:52:07 +0700

    1

--- shoesweb/settings.py
+++ shoesweb/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,7 +55,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    ''
+    'http://localhost:5173/'
 ]
 
 
